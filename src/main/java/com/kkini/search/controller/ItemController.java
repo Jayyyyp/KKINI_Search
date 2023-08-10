ab3059b95677d833e5802715f0f430b924721dfd
Jayyyyp <pjae9726@gmail.com>
2023-08-10 23:12:58 +0900

    search success

--- src/main/java/com/kkini/search/controller/ItemController.java
+++ src/main/java/com/kkini/search/controller/ItemController.java
@@ -3,35 +3,48 @@ package com.kkini.search.controller;
 import com.kkini.search.entity.Item;
 import com.kkini.search.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/items")
 public class ItemController {
-    @Autowired
+
     private ItemService itemService;
 
-    @GetMapping("/searchForm")
-    public String searchForm() {
-        return "searchForm";
+    @Autowired
+    public ItemController (ItemService itemService){
+        this.itemService = itemService;
     }
 
-    @PostMapping("/search")
-    public String search(@RequestParam String name, Model model) {
-        List<Item> items = itemService.searchByName(name);
+    @GetMapping("/search")
+    public String searchItems(@RequestParam(required = false) String name,
+                              @RequestParam(required = false) Long categoryId,
+                              Model model) {
+        List<Item> items;
+
+        if (categoryId != null && categoryId == 1L) {
+            // KKINI 카테고리 선택 시 전체 상품 검색
+            items = itemService.searchItemsByNameAndCategory(name, categoryId);
+        } else if (categoryId != null && categoryId == 2L) {
+            // KKINI Green 카테고리 선택 시 해당 카테고리 상품만 검색
+            items = itemService.searchItemsByNameAndCategory(name, categoryId);
+        } else {
+            // 카테고리 선택이 없는 경우
+            return "search"; // 혹은 다른 로직을 적용하시면 됩니다.
+        }
         model.addAttribute("items", items);
-        return "searchResult";
+
+        if (!items.isEmpty()) {
+            return "searchResults";
+        }else {
+            return "noItems";
+        }
+
     }
-    @GetMapping("/api/search/suggestions")
-    public ResponseEntity<List<String>> getSuggestions(@RequestParam String query, @RequestParam String category) {
-        return ResponseEntity.ok(itemService.findNamesByQueryAndCategory(query, category));
-    }
+
 }
