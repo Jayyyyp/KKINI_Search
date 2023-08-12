f4dbef5741b320b383bfea644bacfbd06a3fe75b
Jayyyyp <pjae9726@gmail.com>
2023-08-12 17:49:02 +0900

    change

--- src/main/java/com/kkini/search/SearchApplication.java
+++ src/main/java/com/kkini/search/SearchApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = "classpath:application-db.properties")
 public class SearchApplication {
 
     public static void main(String[] args) {
